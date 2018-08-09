069fc1bb09292972cb5b7118e517e70731ae464e
zhaofeng <zhaofeng@jiuyescm.com>
2018-08-09 14:28:41 +0800

    新增service

---
+++ bms-base-api/src/main/java/com/jiuyescm/bms/biz/api/IOutstockInfoService.java
@@ -0,0 +1,9 @@
+package com.jiuyescm.bms.biz.api;
+
+import java.util.List;
+
+import com.jiuyescm.bms.biz.vo.OutstockInfoVo;
+
+public interface IOutstockInfoService {
+	public int updateList(List<OutstockInfoVo> list);
+}
---
+++ bms-base-api/src/main/java/com/jiuyescm/bms/biz/api/IOutstockRecordService.java
@@ -0,0 +1,7 @@
+package com.jiuyescm.bms.biz.api;
+
+import com.jiuyescm.bms.biz.vo.OutstockInfoVo;
+
+public interface IOutstockRecordService {
+	public int insert(OutstockInfoVo vo);
+}
