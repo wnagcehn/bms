df138d599d7302bdaf99faf9ecf219836980fd13
zhaofeng <zhaofeng@jiuyescm.com>
2018-08-09 11:14:04 +0800

    规则修改

--- bms-site/src/main/java/com/jiuyescm/bms/chargerule/reciverule/web/ReceiveBillRuleController.java
+++ bms-site/src/main/java/com/jiuyescm/bms/chargerule/reciverule/web/ReceiveBillRuleController.java
@@ -56,13 +56,7 @@ public class ReceiveBillRuleController{
 	 */
 	@DataProvider
 	public void queryAll(Page<BillRuleReceiveEntity> page,Map<String,Object> parameter){
-		if(parameter==null){
-			parameter=new HashMap<String,Object>();		
-			parameter.put("isDefault", "");
-		}	
-		if(parameter!=null && parameter.get("isDefault")==null){
-			parameter.put("isDefault", "");
-		}
+
 		if(parameter!=null && "ALL".equals(parameter.get("bizTypeCode"))){
 			parameter.put("bizTypeCode", "");
 		}
