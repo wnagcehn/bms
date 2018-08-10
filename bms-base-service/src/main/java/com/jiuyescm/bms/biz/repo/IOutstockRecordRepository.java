289e95881680ed7cd5b9ab83cf4e3b0cec3c38b1
zhaofeng <zhaofeng@jiuyescm.com>
2018-08-10 11:52:54 +0800

    批量查询记录

--- bms-base-service/src/main/java/com/jiuyescm/bms/biz/repo/IOutstockRecordRepository.java
+++ bms-base-service/src/main/java/com/jiuyescm/bms/biz/repo/IOutstockRecordRepository.java
@@ -1,10 +1,23 @@
 package com.jiuyescm.bms.biz.repo;
 
 import java.util.List;
+import java.util.Map;
 
+import com.github.pagehelper.PageInfo;
 import com.jiuyescm.bms.biz.entity.BmsOutstockRecordEntity;
 
 public interface IOutstockRecordRepository {
+	
+	/**
+	 * 查询出所有的改动记录
+	 * @param condition
+	 * @param pageNo
+	 * @param pageSize
+	 * @return
+	 */
+    PageInfo<BmsOutstockRecordEntity> query(Map<String, Object> condition, int pageNo,
+            int pageSize);
+	
 	public int insert(BmsOutstockRecordEntity entity);
 	
 	public int insertList(List<BmsOutstockRecordEntity> list);
--- bms-base-service/src/main/java/com/jiuyescm/bms/biz/repo/impl/OutstockRecordRepositoryImpl.java
+++ bms-base-service/src/main/java/com/jiuyescm/bms/biz/repo/impl/OutstockRecordRepositoryImpl.java
@@ -1,9 +1,12 @@
 package com.jiuyescm.bms.biz.repo.impl;
 
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Repository;
 
+import com.github.pagehelper.PageInfo;
 import com.jiuyescm.bms.biz.entity.BmsOutstockRecordEntity;
 import com.jiuyescm.bms.biz.repo.IOutstockRecordRepository;
 import com.jiuyescm.cfm.persistence.mybatis.MyBatisDao;
@@ -14,12 +17,22 @@ public class OutstockRecordRepositoryImpl extends MyBatisDao<BmsOutstockRecordEn
 	@Override
 	public int insert(BmsOutstockRecordEntity entity) {
 		// TODO Auto-generated method stub
-		return this.insert("com.jiuyescm.bms.BmsOutstockRecordEntityMapper.save", entity);
+		return this.insert("com.jiuyescm.bms.BmsOutstockRecordMapper.save", entity);
 	}
 
 	@Override
 	public int insertList(List<BmsOutstockRecordEntity> list) {
 		// TODO Auto-generated method stub
-		return this.insertBatch("com.jiuyescm.bms.BmsOutstockRecordEntityMapper.save", list);
+		return this.insertBatch("com.jiuyescm.bms.BmsOutstockRecordMapper.save", list);
+	}
+
+	@Override
+	public PageInfo<BmsOutstockRecordEntity> query(
+			Map<String, Object> condition, int pageNo, int pageSize) {
+		// TODO Auto-generated method stub
+		 List<BmsOutstockRecordEntity> list = selectList("com.jiuyescm.bms.BmsOutstockRecordMapper.query", condition, new RowBounds(
+	                pageNo, pageSize));
+	     PageInfo<BmsOutstockRecordEntity> pageInfo = new PageInfo<BmsOutstockRecordEntity>(list);
+	     return pageInfo;
 	}
 }
