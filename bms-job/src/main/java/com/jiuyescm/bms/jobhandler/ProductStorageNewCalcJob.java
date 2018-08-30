050b044c33b7876c702c874cd6f00d0f05980ad1
zhaofeng <zhaofeng@jiuyescm.com>
2018-08-30 13:38:16 +0800

    定时任务修改

---
+++ bms-job/src/main/java/com/jiuyescm/bms/jobhandler/ProductStorageNewCalcJob.java
@@ -0,0 +1,412 @@
+package com.jiuyescm.bms.jobhandler;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jiuyescm.bms.base.dictionary.entity.SystemCodeEntity;
+import com.jiuyescm.bms.base.group.service.IBmsGroupCustomerService;
+import com.jiuyescm.bms.base.group.service.IBmsGroupService;
+import com.jiuyescm.bms.base.group.vo.BmsGroupVo;
+import com.jiuyescm.bms.biz.storage.entity.BizProductStorageEntity;
+import com.jiuyescm.bms.calculate.base.IFeesCalcuService;
+import com.jiuyescm.bms.chargerule.receiverule.entity.BillRuleReceiveEntity;
+import com.jiuyescm.bms.common.enumtype.CalculateState;
+import com.jiuyescm.bms.common.enumtype.TemplateTypeEnum;
+import com.jiuyescm.bms.general.entity.FeesReceiveStorageEntity;
+import com.jiuyescm.bms.general.service.IFeesReceiveStorageService;
+import com.jiuyescm.bms.general.service.IPriceContractInfoService;
+import com.jiuyescm.bms.general.service.IStandardReqVoService;
+import com.jiuyescm.bms.general.service.IStorageQuoteFilterService;
+import com.jiuyescm.bms.general.service.SequenceService;
+import com.jiuyescm.bms.quotation.contract.entity.PriceContractInfoEntity;
+import com.jiuyescm.bms.quotation.contract.entity.PriceContractItemEntity;
+import com.jiuyescm.bms.quotation.contract.repository.imp.IPriceContractItemRepository;
+import com.jiuyescm.bms.quotation.storage.entity.PriceGeneralQuotationEntity;
+import com.jiuyescm.bms.quotation.storage.entity.PriceStepQuotationEntity;
+import com.jiuyescm.bms.quotation.storage.repository.IPriceGeneralQuotationRepository;
+import com.jiuyescm.bms.quotation.storage.repository.IPriceStepQuotationRepository;
+import com.jiuyescm.bms.receivable.storage.service.IBizProductStorageService;
+import com.jiuyescm.bms.rule.receiveRule.repository.IReceiveRuleRepository;
+import com.jiuyescm.cfm.common.JAppContext;
+import com.jiuyescm.common.utils.DoubleUtil;
+import com.jiuyescm.contract.quote.api.IContractQuoteInfoService;
+import com.jiuyescm.contract.quote.vo.ContractBizTypeEnum;
+import com.jiuyescm.contract.quote.vo.ContractQuoteInfoVo;
+import com.jiuyescm.contract.quote.vo.ContractQuoteQueryInfoVo;
+import com.jiuyescm.exception.BizException;
+import com.xxl.job.core.handler.annotation.JobHander;
+import com.xxl.job.core.log.XxlJobLogger;
+
+@JobHander(value="productStorageNewCalcJob")
+@Service
+public class ProductStorageNewCalcJob extends CommonJobHandler<BizProductStorageEntity,FeesReceiveStorageEntity>{
+
+	private String SubjectId = "wh_product_storage";		//费用类型-商品存储费
+	
+	@Autowired private IContractQuoteInfoService contractQuoteInfoService;
+	@Autowired private IBizProductStorageService bizProductStorageService;
+	@Autowired private IFeesCalcuService feesCalcuService;
+	@Autowired private IPriceContractInfoService jobPriceContractInfoService;
+	@Autowired private SequenceService sequenceService;
+	@Autowired private IPriceGeneralQuotationRepository priceGeneralQuotationRepository;
+	@Autowired private IPriceStepQuotationRepository  repository;
+	@Autowired private IReceiveRuleRepository receiveRuleRepository;
+	@Autowired private IFeesReceiveStorageService feesReceiveStorageService;
+	@Autowired private IStandardReqVoService standardReqVoServiceImpl;
+	@Autowired private IPriceContractItemRepository priceContractItemRepository;
+	@Autowired private IBmsGroupService bmsGroupService;
+	@Autowired private IBmsGroupCustomerService bmsGroupCustomerService;
+	@Autowired private IStorageQuoteFilterService storageQuoteFilterService;
+	
+	List<SystemCodeEntity> scList;
+	Map<String,PriceGeneralQuotationEntity> mapCusPrice=null;
+	Map<String,PriceStepQuotationEntity> mapCusStepPrice=null;
+	Map<String,PriceContractInfoEntity> mapContact=null;
+	Map<String,BillRuleReceiveEntity> mapRule=null;
+	List<String> cusList=null;
+	String priceType="";
+	
+	@Override
+	protected List<BizProductStorageEntity> queryBillList(Map<String, Object> map) {
+		List<BizProductStorageEntity> bizList = bizProductStorageService.query(map);
+		return bizList;	
+	}
+	
+	@Override
+	protected FeesReceiveStorageEntity initFeeEntity(BizProductStorageEntity entity){		
+		FeesReceiveStorageEntity storageFeeEntity = new FeesReceiveStorageEntity();	
+		storageFeeEntity.setCreator("system");
+		storageFeeEntity.setCreateTime(entity.getCreateTime());
+		storageFeeEntity.setOperateTime(entity.getCreateTime());
+		storageFeeEntity.setCustomerId(entity.getCustomerid());		    //商家ID
+		storageFeeEntity.setCustomerName(entity.getCustomerName());		//商家名称
+		storageFeeEntity.setWarehouseCode(entity.getWarehouseCode());	//仓库ID
+		storageFeeEntity.setWarehouseName(entity.getWarehouseName());	//仓库名称
+		storageFeeEntity.setCostType("FEE_TYPE_GENEARL");			    //费用类别 FEE_TYPE_GENEARL-通用 FEE_TYPE_MATERIAL-耗材 FEE_TYPE_ADD-增值
+		storageFeeEntity.setSubjectCode(SubjectId);		                //费用科目 todo
+		storageFeeEntity.setOtherSubjectCode(SubjectId);
+		storageFeeEntity.setProductType("");							//商品类型
+		if(entity.getAqty()!=null){
+			storageFeeEntity.setQuantity(entity.getAqty());             //商品数量
+		}
+
+		storageFeeEntity.setStatus("0");								//状态
+		storageFeeEntity.setWeight(entity.getWeight());
+		storageFeeEntity.setTempretureType(entity.getTemperature());	//温度类型
+		storageFeeEntity.setProductNo(entity.getProductId());			//商品编码
+		storageFeeEntity.setProductName(entity.getProductName());		//商品名称
+		storageFeeEntity.setUnit("ITEMS");
+		storageFeeEntity.setBizId(String.valueOf(entity.getId()));      //业务数据主键
+		storageFeeEntity.setCost(new BigDecimal(0));					//入仓金额
+		storageFeeEntity.setUnitPrice(0d);
+		storageFeeEntity.setFeesNo(entity.getFeesNo());		
+		storageFeeEntity.setParam1(TemplateTypeEnum.COMMON.getCode());
+		storageFeeEntity.setDelFlag("0");
+		return storageFeeEntity;
+	}
+
+	@Override
+	protected boolean isNoExe(BizProductStorageEntity entity,FeesReceiveStorageEntity feeEntity) {
+		//指定的商家
+		Map<String,Object> map= new HashMap<String, Object>();
+		map.put("groupCode", "customer_unit");
+		map.put("bizType", "group_customer");
+		BmsGroupVo bmsGroup=bmsGroupService.queryOne(map);
+		if(bmsGroup!=null){
+			cusList=bmsGroupCustomerService.queryCustomerByGroupId(bmsGroup.getId());
+		}
+		
+		//只有按件商家才计算
+		if(cusList.size()==0 || !cusList.contains(entity.getCustomerid())){
+			XxlJobLogger.log("只有按件商家收取按件存储费,其他不计费");
+			entity.setIsCalculated(CalculateState.No_Exe.getCode());
+			feeEntity.setIsCalculated(CalculateState.No_Exe.getCode());
+			entity.setRemark("只有按件商家收取按件存储费,其他不计费");
+			return true;
+		}
+		return false;
+	}
+	
+	
+	@Override
+	protected ContractQuoteInfoVo getContractForWhat(BizProductStorageEntity entity) {
+		ContractQuoteQueryInfoVo queryVo = new ContractQuoteQueryInfoVo();
+		queryVo.setCustomerId(entity.getCustomerid());
+		queryVo.setBizTypeCode(ContractBizTypeEnum.STORAGE.getCode());
+		queryVo.setSubjectCode(SubjectId);
+		queryVo.setCurrentTime(entity.getCreateTime());
+		queryVo.setWarehouseCode(entity.getWarehouseCode());
+		
+		ContractQuoteInfoVo modelEntity = new ContractQuoteInfoVo();
+		try{
+			modelEntity = contractQuoteInfoService.queryUniqueColumns(queryVo);
+		}
+		catch(BizException ex){
+			XxlJobLogger.log("合同在线无此合同",ex);
+		}
+		return modelEntity;
+	}
+	
+	@Override
+	protected void calcuForBms(BizProductStorageEntity entity,FeesReceiveStorageEntity feeEntity){
+		//合同报价校验  false-不通过  true-通过
+		if(validateData(entity, feeEntity)){
+			if(mapCusPrice.containsKey(entity.getCustomerid())){
+				entity.setCalculateTime(JAppContext.currentTimestamp());
+				feeEntity.setCalculateTime(entity.getCalculateTime());
+				
+				String customerId=entity.getCustomerid();
+				//报价模板
+				PriceGeneralQuotationEntity generalEntity=mapCusPrice.get(customerId);	
+				if("ITEMS".equals(generalEntity.getFeeUnitCode())){//按件
+					feeEntity.setQuantity(entity.getAqty());
+				}else if("KILOGRAM".equals(generalEntity.getFeeUnitCode())){//按重量
+					feeEntity.setQuantity(entity.getWeight()*entity.getAqty());
+				}
+
+				//计算方法
+				double amount=0d;
+				switch(priceType){
+				case "PRICE_TYPE_NORMAL"://一口价				
+		            // -> 费用 = 数量*模板单价
+					if("ITEMS".equals(generalEntity.getFeeUnitCode())){//按件
+						amount=entity.getAqty()*generalEntity.getUnitPrice();
+					}else if("KILOGRAM".equals(generalEntity.getFeeUnitCode())){//按重量
+						amount=entity.getWeight()*entity.getAqty()*generalEntity.getUnitPrice();
+					}
+					feeEntity.setUnitPrice(generalEntity.getUnitPrice());
+					feeEntity.setParam3(generalEntity.getId()+"");
+					break;
+				case "PRICE_TYPE_STEP"://阶梯价
+					PriceStepQuotationEntity stepQuoEntity=mapCusStepPrice.get(customerId);				
+					if("ITEMS".equals(generalEntity.getFeeUnitCode())){//按件
+						if(!DoubleUtil.isBlank(stepQuoEntity.getUnitPrice())){
+							feeEntity.setUnitPrice(stepQuoEntity.getUnitPrice());
+							amount=entity.getAqty()*stepQuoEntity.getUnitPrice();
+						}else{
+							amount=stepQuoEntity.getFirstNum()<entity.getAqty()?stepQuoEntity.getFirstPrice()+(entity.getAqty()-stepQuoEntity.getFirstNum())/stepQuoEntity.getContinuedItem()*stepQuoEntity.getContinuedPrice():stepQuoEntity.getFirstPrice();
+						}
+					}else if("KILOGRAM".equals(generalEntity.getFeeUnitCode())){//按重量
+						if(!DoubleUtil.isBlank(stepQuoEntity.getUnitPrice())){
+							feeEntity.setUnitPrice(stepQuoEntity.getUnitPrice());
+							amount=entity.getWeight()*entity.getAqty()*stepQuoEntity.getUnitPrice();
+						}else{
+							amount=stepQuoEntity.getFirstNum()<entity.getWeight()*entity.getAqty()?stepQuoEntity.getFirstPrice()+(entity.getWeight()*entity.getAqty()-stepQuoEntity.getFirstNum())/stepQuoEntity.getContinuedItem()*stepQuoEntity.getContinuedPrice():stepQuoEntity.getFirstPrice();
+						}
+					}
+					//判断封顶价
+					if(!DoubleUtil.isBlank(stepQuoEntity.getCapPrice())){
+						if(stepQuoEntity.getCapPrice()<amount){
+							amount=stepQuoEntity.getCapPrice();
+						}
+					}
+					feeEntity.setParam3(stepQuoEntity.getId()+"");
+					break;
+				default:
+					break;
+				}
+				
+				feeEntity.setCost(BigDecimal.valueOf(amount));
+				feeEntity.setParam4(priceType);
+				feeEntity.setBizType(entity.getextattr1());//判断是否是遗漏数据
+				entity.setRemark("计算成功");
+				entity.setIsCalculated(CalculateState.Finish.getCode());
+				feeEntity.setIsCalculated(CalculateState.Finish.getCode());				
+				XxlJobLogger.log(String.format("====================================[%s]计算完毕============================================",entity.getId()));
+			}
+		}
+	}
+
+	@Override
+	protected void calcuForContract(BizProductStorageEntity entity,FeesReceiveStorageEntity feeEntity){
+		try{
+			Map<String, Object> con = new HashMap<>();
+			con.put("quotationNo", contractQuoteInfoVo.getRuleCode());
+			BillRuleReceiveEntity ruleEntity = receiveRuleRepository.queryOne(con);
+			//获取合同在线查询条件
+			Map<String, Object> cond = feesCalcuService.ContractCalcuService(entity, contractQuoteInfoVo.getUniqueMap(), ruleEntity.getRule(), ruleEntity.getQuotationNo());
+			ContractQuoteInfoVo rtnQuoteInfoVo = contractQuoteInfoService.queryQuotes(contractQuoteInfoVo, cond);
+			for (Map<String, String> map : rtnQuoteInfoVo.getQuoteMaps()) {
+				XxlJobLogger.log("报价信息 -- "+map);
+			}
+			//调用规则计算费用
+			Map<String, Object> feesMap = feesCalcuService.ContractCalcuService(feeEntity, rtnQuoteInfoVo.getQuoteMaps(), ruleEntity.getRule(), ruleEntity.getQuotationNo());
+
+			if(feeEntity.getCost().compareTo(BigDecimal.ZERO) == 1){
+				feeEntity.setIsCalculated(CalculateState.Finish.getCode());
+				entity.setIsCalculated(CalculateState.Finish.getCode());
+				XxlJobLogger.log("计算成功，费用【{0}】",feeEntity.getCost());
+			}
+			else{
+				feeEntity.setIsCalculated(CalculateState.Quote_Miss.getCode());
+				entity.setIsCalculated(CalculateState.Quote_Miss.getCode());
+				XxlJobLogger.log("计算不成功，费用【0】");
+			}
+		}
+		catch(Exception ex){
+			feeEntity.setIsCalculated(CalculateState.Sys_Error.getCode());
+			entity.setIsCalculated(CalculateState.Sys_Error.getCode());
+			XxlJobLogger.log("计算不成功，费用【0】");
+		}
+		
+	}
+
+	
+	
+	@Override
+	protected void updateBatch(List<BizProductStorageEntity> ts,List<FeesReceiveStorageEntity> fs) {
+
+		long start = System.currentTimeMillis();// 系统开始时间
+		long current = 0l;// 当前系统时间
+		bizProductStorageService.updateBatch(ts);
+		current = System.currentTimeMillis();
+		XxlJobLogger.log("更新业务数据耗时：【{0}】毫秒",(current - start));
+		start = System.currentTimeMillis();// 系统开始时间
+		feesReceiveStorageService.updateBatch(fs);
+		current = System.currentTimeMillis();
+		XxlJobLogger.log("新增费用数据耗时：【{0}】毫秒  ",(current - start));
+		
+	}
+
+	/**
+	 * 验证业务数据
+	 */
+	protected boolean validateData(BizProductStorageEntity entity,
+			FeesReceiveStorageEntity fee) {
+		
+		mapCusStepPrice=new HashMap<String,PriceStepQuotationEntity>();
+		mapCusPrice=new HashMap<String,PriceGeneralQuotationEntity>();
+		mapContact=new HashMap<String,PriceContractInfoEntity>();
+		mapRule=new HashMap<String,BillRuleReceiveEntity>();
+		
+		
+		XxlJobLogger.log("数据主键ID:【{0}】  ",entity.getId());
+		entity.setCalculateTime(JAppContext.currentTimestamp());
+		Map<String,Object> map=new HashMap<String,Object>();
+		String customerId=entity.getCustomerid();
+		FeesReceiveStorageEntity storageFeeEntity = initFeeEntity(entity);
+		storageFeeEntity.setCalculateTime(JAppContext.currentTimestamp());
+		
+		long start = System.currentTimeMillis();// 系统开始时间
+		long current = 0l;// 当前系统时间
+		
+		/*验证商家是否合同存在*/
+		PriceContractInfoEntity contractEntity=null;
+		if(mapContact.containsKey(entity.getCustomerid())){
+			contractEntity=mapContact.get(entity.getCustomerid());
+		}else{
+			Map<String,Object> aCondition=new HashMap<>();
+			aCondition.put("customerid", entity.getCustomerid());
+			aCondition.put("contractTypeCode", "CUSTOMER_CONTRACT");
+		    contractEntity = jobPriceContractInfoService.queryContractByCustomer(aCondition);
+		    mapContact.put(entity.getCustomerid(), contractEntity);
+		}
+		
+		if(contractEntity == null || StringUtils.isEmpty(contractEntity.getContractCode())){
+			XxlJobLogger.log(String.format("未查询到合同  订单号【%s】--商家【%s】", entity.getId(),entity.getCustomerid()));
+			entity.setIsCalculated(CalculateState.Contract_Miss.getCode());
+			storageFeeEntity.setIsCalculated(CalculateState.Contract_Miss.getCode());
+			entity.setRemark(String.format("未查询到合同  订单号【%s】--商家【%s】", entity.getId(),entity.getCustomerid()));
+			return false;
+		}
+		current = System.currentTimeMillis();
+		XxlJobLogger.log("验证合同   耗时【{0}】毫秒  合同编号 【{1}】",(current - start),contractEntity.getContractCode());
+		
+		//----验证签约服务
+		start = System.currentTimeMillis();// 系统开始时间
+		Map<String,Object> contractItems_map=new HashMap<String,Object>();
+		contractItems_map.put("contractCode", contractEntity.getContractCode());
+		contractItems_map.put("subjectId", SubjectId);
+		List<PriceContractItemEntity> contractItems = priceContractItemRepository.query(contractItems_map);
+		if(contractItems == null || contractItems.size() == 0 || StringUtils.isEmpty(contractItems.get(0).getTemplateId())) {
+			XxlJobLogger.log("未签约服务  订单号【{0}】--商家【{1}】", entity.getId(),entity.getCustomerid());
+			entity.setIsCalculated(CalculateState.Contract_Miss.getCode());
+			storageFeeEntity.setIsCalculated(CalculateState.Contract_Miss.getCode());
+			entity.setRemark("未签约服务");
+			return false;
+		}
+		current = System.currentTimeMillis();
+		XxlJobLogger.log("验证签约服务耗时：【{0}】毫秒  ",(current - start));				
+		
+		
+		start = System.currentTimeMillis();// 系统开始时间
+		/*验证报价 报价*/
+		PriceGeneralQuotationEntity quoTemplete=null;
+		if(!mapCusPrice.containsKey(customerId)){
+			map.clear();
+			map.put("subjectId",SubjectId);
+			map.put("quotationNo", contractItems.get(0).getTemplateId());
+			quoTemplete=priceGeneralQuotationRepository.query(map);
+			if(quoTemplete != null){
+				mapCusPrice.put(customerId, quoTemplete);//加入缓存
+			}
+		}else{
+			quoTemplete=mapCusPrice.get(entity.getCustomerid());
+		}
+		if(quoTemplete==null){
+			XxlJobLogger.log("报价未配置");
+			entity.setIsCalculated(CalculateState.Quote_Miss.getCode());
+			storageFeeEntity.setIsCalculated(CalculateState.Quote_Miss.getCode());
+			entity.setRemark("报价未配置");
+			return false;
+		}
+		PriceGeneralQuotationEntity priceGeneral=quoTemplete;
+	    priceType=priceGeneral.getPriceType();
+		List<PriceStepQuotationEntity> list=new ArrayList<PriceStepQuotationEntity>();
+		PriceStepQuotationEntity price=new PriceStepQuotationEntity();
+		if(priceType.equals("PRICE_TYPE_STEP")){//阶梯价格
+			//寻找阶梯报价
+			map.clear();
+			map.put("quotationId", priceGeneral.getId());
+			//根据报价单位判断
+			if("ITEMS".equals(priceGeneral.getFeeUnitCode())){//按件
+				map.put("num", entity.getAqty());	
+			}else if("KILOGRAM".equals(priceGeneral.getFeeUnitCode())){//按重量
+				map.put("num", entity.getWeight()*entity.getAqty());
+			}
+					
+			//查询出的所有子报价
+			list=repository.queryPriceStepByQuatationId(map);
+			
+			if(list==null || list.size() == 0){
+				XxlJobLogger.log("阶梯报价未配置");
+				entity.setIsCalculated(CalculateState.Quote_Miss.getCode());
+				storageFeeEntity.setIsCalculated(CalculateState.Quote_Miss.getCode());
+				entity.setRemark("阶梯报价未配置");
+				return  false;
+			}
+			
+			//封装数据的仓库和温度
+			map.clear();
+			map.put("warehouse_code", entity.getWarehouseCode());
+			price=storageQuoteFilterService.quoteFilter(list, map);
+			mapCusStepPrice.put(customerId,price);
+			if(price==null){
+				XxlJobLogger.log("阶梯报价未配置");
+				entity.setIsCalculated(CalculateState.Quote_Miss.getCode());
+				storageFeeEntity.setIsCalculated(CalculateState.Quote_Miss.getCode());
+				entity.setRemark("阶梯报价未配置");
+				return  false;
+			}
+		}else if(priceType.equals("PRICE_TYPE_NORMAL")){//一口价
+			
+		}else{//报价类型缺失
+			XxlJobLogger.log("报价类型未知");
+			entity.setIsCalculated(CalculateState.Quote_Miss.getCode());
+			storageFeeEntity.setIsCalculated(CalculateState.Quote_Miss.getCode());
+			entity.setRemark("报价【"+priceGeneral.getQuotationNo()+"】类型未知");
+			return  false;
+		}
+		current = System.currentTimeMillis();
+		XxlJobLogger.log("验证报价耗时：【{0}】毫秒  ",(current - start));
+		return true;
+	}
+	
+}
