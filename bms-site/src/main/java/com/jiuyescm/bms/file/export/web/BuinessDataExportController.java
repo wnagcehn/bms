ca190ea7d30fb27c6b9d7e80929c049d5e1add11
zhaofeng <zhaofeng@jiuyescm.com>
2018-09-12 15:51:47 +0800

    预账单导出修改

--- bms-site/src/main/java/com/jiuyescm/bms/file/export/web/BuinessDataExportController.java
+++ bms-site/src/main/java/com/jiuyescm/bms/file/export/web/BuinessDataExportController.java
@@ -270,15 +270,15 @@ public class BuinessDataExportController extends BaseController {
 
 		// 配送费
 		handDispatch(xssfWorkbook, poiUtil, condition, filePath);
-		
-		handOutstock(xssfWorkbook, poiUtil, condition, filePath);
-		
+				
 		updateExportTask(taskId, FileTaskStateEnum.INPROCESS.getCode(), 50);
 
 		List<String> warehouseList = queryPreBillWarehouse(condition);
+		Map<String, String> temMap=getTemperatureTypeList();
 		// 存储费
 		handStorage(xssfWorkbook, condition, poiUtil, warehouseList);
-		
+		//出库
+		handOutstock(xssfWorkbook, poiUtil, condition, filePath,temMap);
 		//入库
 		handInstock(xssfWorkbook, poiUtil, condition, filePath);
 		
@@ -1061,7 +1061,7 @@ public class BuinessDataExportController extends BaseController {
 	 * @throws Exception
 	 */
 	private void handOutstock(SXSSFWorkbook xssfWorkbook, POISXSSUtil poiUtil, Map<String, Object> condition,
-			String filePath) throws Exception {
+			String filePath,Map<String, String> temMap) throws Exception {
 		int pageNo = 1;
 		int lineNo = 1;
 		boolean doLoop = true;
@@ -1080,7 +1080,7 @@ public class BuinessDataExportController extends BaseController {
 			
 			//头、内容信息
 			List<Map<String, Object>> headDetailMapList = getOutHead(); 
-			List<Map<String, Object>> dataDetailList = getOutHeadItem(pageInfo.getList());
+			List<Map<String, Object>> dataDetailList = getOutHeadItem(pageInfo.getList(),temMap);
 			
 			poiUtil.exportExcel2FilePath(poiUtil, xssfWorkbook, FileTaskTypeEnum.BIZ_PRO_OUTSTOCK.getDesc(), 
 					lineNo, headDetailMapList, dataDetailList);
@@ -1167,7 +1167,7 @@ public class BuinessDataExportController extends BaseController {
         return headInfoList;
 	}
 	
-	private List<Map<String, Object>> getOutHeadItem(List<FeesReceiveStorageEntity> list){
+	private List<Map<String, Object>> getOutHeadItem(List<FeesReceiveStorageEntity> list,Map<String, String> temMap){
 		 	List<Map<String, Object>> dataList = new ArrayList<Map<String,Object>>();
 	        Map<String, Object> dataItem = null;
 	        Map<String,Map<String, Object>> entityMap=new HashMap<String,Map<String, Object>>();
@@ -1179,7 +1179,7 @@ public class BuinessDataExportController extends BaseController {
 		        	dataItem.put("customerName", entity.getCustomerName() );
 		        	dataItem.put("orderType", entity.getOrderType());
 		        	dataItem.put("outstockNo", entity.getOutstockNo());
-		        	dataItem.put("tempretureType", entity.getTempretureType());
+		        	dataItem.put("tempretureType", temMap.get(entity.getTempretureType()));
 	        		//B2B订单操作费（区分是订单操作费还是出库装车费）
 	        		if("wh_b2b_work".equals(entity.getSubjectCode())){
 	        			dataItem.put("orderCost", entity.getCost());
@@ -2068,6 +2068,22 @@ public class BuinessDataExportController extends BaseController {
 		}
 	}
 
+	/**
+	 * 温度类型
+	 * @return
+	 */
+	@DataProvider
+	public Map<String, String> getTemperatureTypeList(){
+		List<SystemCodeEntity> systemCodeList = systemCodeService.findEnumList("TEMPERATURE_TYPE");
+		Map<String, String> map =new LinkedHashMap<String,String>();
+		if(systemCodeList!=null && systemCodeList.size()>0){
+			for(int i=0;i<systemCodeList.size();i++){
+				map.put(systemCodeList.get(i).getCode(), systemCodeList.get(i).getCodeName());
+			}
+		}
+		return map;
+	}
+	
 	private void setColumnWeight(Sheet sheet, int len, int weight) {
 		if (len > 0) {
 			for (int i = 0; i < len; i++) {
