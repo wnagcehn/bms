ffce989157cb87b1b9aed7c3185771fac67799b7
zhaofeng <zhaofeng@jiuyescm.com>
2018-09-14 15:41:21 +0800

    新增日志

--- bms-job/src/main/java/com/jiuyescm/bms/jobhandler/DispatchBillNewCalcJob.java
+++ bms-job/src/main/java/com/jiuyescm/bms/jobhandler/DispatchBillNewCalcJob.java
@@ -656,7 +656,7 @@ public class DispatchBillNewCalcJob extends CommonJobHandler<BizDispatchBillEnti
 		try{
 			modelEntity = contractQuoteInfoService.queryUniqueColumns(queryVo);
 			
-			XxlJobLogger.log("-->"+entity.getId()+"查询出的合同在线结果",JSONObject.fromObject(modelEntity));
+			XxlJobLogger.log("-->"+entity.getId()+"查询出的合同在线结果"+JSONObject.fromObject(modelEntity));
 		}
 		catch(BizException ex){
 			XxlJobLogger.log("-->"+entity.getId()+"合同在线无此合同--"+ex.getMessage());
