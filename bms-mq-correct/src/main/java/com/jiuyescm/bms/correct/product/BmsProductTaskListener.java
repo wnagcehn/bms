fec5130ce6803df90af10e02dcedd062118f1125
zhaofeng <zhaofeng@jiuyescm.com>
2018-11-15 11:47:10 +0800

    1.折扣和纠正新增日志 2.折扣页面修改

--- bms-mq-correct/src/main/java/com/jiuyescm/bms/correct/product/BmsProductTaskListener.java
+++ bms-mq-correct/src/main/java/com/jiuyescm/bms/correct/product/BmsProductTaskListener.java
@@ -12,6 +12,8 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
+import net.sf.json.JSONObject;
+
 import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
@@ -72,27 +74,27 @@ public class BmsProductTaskListener implements MessageListener{
 		
 		try {
 			StringBuffer errorMessage=new StringBuffer();
-			logger.info("正在消费"+taskId);
+			logger.info(taskId+"正在消费");
 			//处理运单重量统一
-			logger.info("正在处理运单重量纠正");
+			logger.info(taskId+"正在处理运单重量纠正");
 			start = System.currentTimeMillis();
 			handWeightTask(taskId,errorMessage);
 			logger.info("重量纠正结束 耗时--"+(System.currentTimeMillis()-start));
 			
 			
 			//处理运单耗材统一
-			logger.info("正在处理耗材纠正");
+			logger.info(taskId+"正在处理耗材纠正");
 			start = System.currentTimeMillis();
 			handMaterialTask(taskId,errorMessage);
-			logger.info("耗材纠正结束 耗时--"+(System.currentTimeMillis()-start));
+			logger.info(taskId+"耗材纠正结束 耗时--"+(System.currentTimeMillis()-start));
 			
 			//处理运单保温袋统一
-			logger.info("正在处理保温袋纠正");
+			logger.info(taskId+"正在处理保温袋纠正");
 			start = System.currentTimeMillis();
 			handBwdTask(taskId,errorMessage);
-			logger.info("保温袋纠正结束 耗时--"+(System.currentTimeMillis()-start));
+			logger.info(taskId+"保温袋纠正结束 耗时--"+(System.currentTimeMillis()-start));
 		} catch (Exception e1) {
-			logger.error("处理运单重量和耗材统一失败：{}",e1);
+			logger.error(taskId+"处理运单重量和耗材统一失败：{}",e1);
 			return;
 		}
 		
@@ -118,8 +120,8 @@ public class BmsProductTaskListener implements MessageListener{
 		condition.put("taskId", taskId);
 		List<BmsCorrectAsynTaskVo> taskList=bmsCorrectAsynTaskService.queryList(condition);
 		if(taskList.size()<=0){
-			logger.info("没有查询到重量纠正任务记录;");
-			errorMessage.append("没有查询到重量纠正任务记录;");
+			logger.info(taskId+"没有查询到重量纠正任务记录;");
+			errorMessage.append(taskId+"没有查询到重量纠正任务记录;");
 			return;
 		}
 		BmsCorrectAsynTaskVo taskVo=taskList.get(0);
@@ -140,14 +142,14 @@ public class BmsProductTaskListener implements MessageListener{
 			taskVo.setRemark(errorMessage.toString());
 			int resultTask=bmsCorrectAsynTaskService.update(taskVo);
 			if(resultTask<=0){
-				logger.info("运单重量纠正任务状态更新失败");
+				logger.info(taskId+"运单重量纠正任务状态更新失败");
 				errorMessage.append("运单重量纠正任务状态更新失败;");
 				taskVo.setRemark(errorMessage.toString());
 				taskVo.setTaskStatus(BmsCorrectAsynTaskStatusEnum.FAIL.getCode());
 				bmsCorrectAsynTaskService.update(taskVo);
 			}		
 		} catch (Exception e) {
-			logger.error("重量调整异常，错误日志：{}",e);
+			logger.error(taskId+"重量调整异常，错误日志：{}",e);
 			errorMessage.append("重量调整异常;");
 			taskVo.setRemark(errorMessage.toString());
 			taskVo.setTaskRate(35);
@@ -169,22 +171,25 @@ public class BmsProductTaskListener implements MessageListener{
 			condition.put("endTime", DateUtil.formatTimestamp(taskVo.getEndDate()));
 			condition.put("taskId", taskId);
 			updateProgress(taskVo,10);
-			logger.info("正在进行重量汇总统计 ");
+			logger.info(taskId+"正在进行重量汇总统计 ");
 			logger.info(condition);
+			logger.info(taskId+"统计的参数"+JSONObject.fromObject(condition));		
 			//插入汇总统计
 			start = System.currentTimeMillis();
 			int re=bmsProductsWeightService.saveWeight(condition);
 			if(re<=0){
-				logger.info("插入重量汇总统计失败");
+				logger.info(taskId+"插入重量汇总统计失败");
 				errorMessage.append("插入重量汇总统计失败");
 				return "fail";
 			}
-			logger.info("重量汇总统计 耗时--" + (System.currentTimeMillis()-start));
+			logger.info(taskId+"重量汇总统计 耗时--" + (System.currentTimeMillis()-start));
 			//获取商品明细组合与重量占比最高
 			condition.put("taskId", taskId);
 			
 			
 			List<BmsProductsWeightAccountVo> list=bmsProductsWeightService.queyAllMax(condition);
+			logger.info(taskId+"占比最高的记录"+JSONObject.fromObject(list));		
+
 			if(list.size()>0){
 				//循环更新每个商品明细对应的运单
 				for(int i=0;i<list.size();i++){
@@ -207,7 +212,7 @@ public class BmsProductTaskListener implements MessageListener{
 							}
 						}
 
-						logger.info("查询出该商品明细下不是这个重量的所有打标记录");
+						logger.info(taskId+"查询出该商品明细下不是这个重量的所有打标记录");
 						
 						String productDetail=proAccountVo.getProductsMark();
 						condition=new HashMap<String,Object>();
@@ -216,7 +221,7 @@ public class BmsProductTaskListener implements MessageListener{
 						condition.put("startTime", DateUtil.formatTimestamp(taskVo.getStartDate()));
 						condition.put("endTime", DateUtil.formatTimestamp(taskVo.getEndDate()));
 						
-						logger.info("商品明细"+productDetail+"的标准重量"+newWeight);
+						logger.info(taskId+"商品明细"+productDetail+"的标准重量"+newWeight);
 						
 						//查询出该商品明细下不是这个重量的所有打标记录
 						List<BmsMarkingProductsVo> marklist=bmsProductsWeightService.queryMark(condition);
@@ -226,19 +231,19 @@ public class BmsProductTaskListener implements MessageListener{
 								waybillNoList.add(vo.getWaybillNo());
 							}
 							//批量更新打标记录
-							logger.info("批量更新打标记录 运单数--"+waybillNoList.size());
+							logger.info(taskId+"批量更新打标记录 运单数--"+waybillNoList.size());
 							int result=bmsProductsWeightService.updateMark(condition);
 							if(result>0){
 								//去更新运单状态
 								start = System.currentTimeMillis();
 								int result2=bizDispatchBillService.retryByWaybillNo(waybillNoList);
 								end = System.currentTimeMillis();
-								logger.info("------------------更新运单状态耗时：" + (end-start) + "毫秒------------------");
+								logger.info(taskId+"------------------更新运单状态耗时：" + (end-start) + "毫秒------------------");
 								if(result2<=0){						
-									logger.info("更新运单失败");
+									logger.info(taskId+"更新运单失败");
 								}
 							}else{
-								logger.info("批量更新重量打标记录失败");
+								logger.info(taskId+"批量更新重量打标记录失败");
 							}
 						}
 						
@@ -259,7 +264,7 @@ public class BmsProductTaskListener implements MessageListener{
 		condition.put("taskId", taskId);
 		List<BmsCorrectAsynTaskVo> taskList=bmsCorrectAsynTaskService.queryList(condition);
 		if(taskList.size()<=0){
-			logger.info("耗材调整,没有查询到任务记录;");
+			logger.info(taskId+"耗材调整,没有查询到任务记录;");
 			errorMessage.append("耗材调整,没有查询到任务记录;");
 			return;
 		}
@@ -267,7 +272,7 @@ public class BmsProductTaskListener implements MessageListener{
 		try{
 			String result=handMaterial(taskVo,taskId,errorMessage);		
 			if("sucess".equals(result)){
-				logger.info("耗材调整成功");
+				logger.info(taskId+"耗材调整成功");
 				taskVo.setTaskRate(70);
 				errorMessage.append("耗材调整成功;");
 				taskVo.setTaskStatus(BmsCorrectAsynTaskStatusEnum.SUCCESS.getCode());
@@ -284,7 +289,7 @@ public class BmsProductTaskListener implements MessageListener{
 				bmsCorrectAsynTaskService.update(taskVo);				
 			}
 		} catch (Exception e) {
-			logger.error("耗材调整异常，错误日志：{}",e);
+			logger.error(taskId+"耗材调整异常，错误日志：{}",e);
 			errorMessage.append("耗材调整异常;");
 			taskVo.setRemark(errorMessage.toString());
 			taskVo.setTaskRate(75);
@@ -301,7 +306,7 @@ public class BmsProductTaskListener implements MessageListener{
 		Map<String,Object> condition=new HashMap<String,Object>();
 		if(StringUtils.isNotBlank(taskVo.getCustomerId())){
 			//根据商家和时间查询耗材出库表业务数据里的运单号
-			logger.info("查询耗材汇总统计");
+			logger.info(taskId+"查询耗材汇总统计");
 			condition=new HashMap<String,Object>();
 			condition.put("customerId", taskVo.getCustomerId());
 			condition.put("startTime", DateUtil.formatTimestamp(taskVo.getStartDate()));
@@ -309,24 +314,24 @@ public class BmsProductTaskListener implements MessageListener{
 			condition.put("taskId", taskId);
 			condition.put("type", "PMXZX");
 			
-			logger.info("插入汇总统计");
+			logger.info(taskId+"插入汇总统计"+JSONObject.fromObject(condition));		
 			//插入汇总统计
 			long sta = System.currentTimeMillis();
 			int re=bmsProductsMaterialService.saveMaterial(condition);
 			long en = System.currentTimeMillis();
 			if(re<=0){
 				errorMessage.append("耗材汇总统计失败;");
-				logger.info("耗材汇总统计失败");
+				logger.info(taskId+"耗材汇总统计失败");
 				return "fail";
 			}
 			updateProgress(taskVo, 50);
-			logger.info("获取占比最高的耗材");
+			logger.info(taskId+"获取占比最高的耗材");
 			//获取占比最高
 			condition.put("taskId", taskId);
 			start = System.currentTimeMillis();
 			List<BmsProductsMaterialAccountVo> list=bmsProductsMaterialService.queyAllMax(condition);
 			end = System.currentTimeMillis();
-			logger.info("------------------获取占比最高耗时：" + (end-start) + "毫秒------------------");
+			logger.info(taskId+"------------------获取占比最高耗时：" + (end-start) + "毫秒------------------");
 			//循环判断是否有重复之
 			if(list.size()>0){
 				//循环更新每个商品明细对应的耗材
@@ -347,9 +352,10 @@ public class BmsProductTaskListener implements MessageListener{
 						
 						//查询出该商品明细下使用最多的运单打标记录
 						start = System.currentTimeMillis();
+						logger.info(taskId+"查询出该商品明细下使用最多的运单打标参数"+JSONObject.fromObject(condition));		
 						BmsMarkingMaterialVo markVo=bmsProductsMaterialService.queryOneMark(condition);
 						end = System.currentTimeMillis();
-						logger.info("------------------查询出该商品明细下使用最多的运单打标记录耗时：" + (end-start) + "毫秒------------------");
+						logger.info(taskId+"------------------查询出该商品明细下使用最多的运单打标记录耗时：" + (end-start) + "毫秒------------------");
 						if(markVo!=null){
 							waybillNo=markVo.getWaybillNo();
 						}
@@ -364,11 +370,13 @@ public class BmsProductTaskListener implements MessageListener{
 							condition.put("waybillNo", waybillNo);
 							condition.put("type", "PMXZX");
 							start = System.currentTimeMillis();
+							logger.info(taskId+"查询出该商品明细下使用最多的运单打标参数"+JSONObject.fromObject(condition));		
+
 							int updateResult=bmsProductsMaterialService.updateMaterialAccount(condition);
 							end = System.currentTimeMillis();
-							logger.info("------------------将占比最高的运单号更新到汇总表里耗时：" + (end-start) + "毫秒------------------");
+							logger.info(taskId+"------------------将占比最高的运单号更新到汇总表里耗时：" + (end-start) + "毫秒------------------");
 							if(updateResult<=0){
-								logger.info("更新汇总表失败"+condition);
+								logger.info(taskId+"更新汇总表失败"+condition);
 								continue;
 							}
 													
@@ -379,7 +387,7 @@ public class BmsProductTaskListener implements MessageListener{
 							start = System.currentTimeMillis();
 							List<BizOutstockPackmaterialEntity> standMaterial=bizOutstockPackmaterialService.queryMaterial(condition);
 							end = System.currentTimeMillis();
-							logger.info("------------------查询该运单使用到的标准泡沫箱和纸箱耗时：" + (end-start) + "毫秒------------------");
+							logger.info(taskId+"------------------查询该运单使用到的标准泡沫箱和纸箱耗时：" + (end-start) + "毫秒------------------");
 							//未查询到耗材
 							if(standMaterial.size()==0){
 								continue;
@@ -393,9 +401,10 @@ public class BmsProductTaskListener implements MessageListener{
 							condition.put("startTime", DateUtil.formatTimestamp(taskVo.getStartDate()));
 							condition.put("endTime", DateUtil.formatTimestamp(taskVo.getEndDate()));
 							start = System.currentTimeMillis();
+							logger.info(taskId+"找出未使用标准的运单号参数"+JSONObject.fromObject(condition));
 							List<BmsMarkingMaterialVo> notMaxList=bmsProductsMaterialService.queyNotMax(condition);
 							end = System.currentTimeMillis();
-							logger.info("------------------找出未使用标准的运单号耗时：" + (end-start) + "毫秒------------------");
+							logger.info(taskId+"------------------找出未使用标准的运单号耗时：" + (end-start) + "毫秒------------------");
 							
 							long total = 0l;
 							long delTimeTotal = 0l;
@@ -419,7 +428,7 @@ public class BmsProductTaskListener implements MessageListener{
 									end = System.currentTimeMillis();
 									delTimeTotal = delTimeTotal + (end-start);
 									if(result<=0){
-										logger.info("删除占比小耗材失败");
+										logger.info(taskId+"删除占比小耗材失败");
 										continue;
 									}
 									
@@ -458,8 +467,8 @@ public class BmsProductTaskListener implements MessageListener{
 									}															
 								}							
 							}
-							logger.info("------------------删除原运单号对应得耗材和费用总耗时：" + delTimeTotal + "毫秒------------------");
-							logger.info("------------------保存新耗材成功，总耗时：" + total + "毫秒------------------");
+							logger.info(taskId+"------------------删除原运单号对应得耗材和费用总耗时：" + delTimeTotal + "毫秒------------------");
+							logger.info(taskId+"------------------保存新耗材成功，总耗时：" + total + "毫秒------------------");
 						}
 						condition=new HashMap<String,Object>();
 						condition.put("productsMark", proAccountVo.getProductsMark());
@@ -473,11 +482,11 @@ public class BmsProductTaskListener implements MessageListener{
 					}
 				}		
 			}
-			logger.info("------------------插入汇总统计运单:"+re+"条,耗时：" + (en-sta) + "毫秒------------------");
+			logger.info(taskId+"------------------插入汇总统计运单:"+re+"条,耗时：" + (en-sta) + "毫秒------------------");
 		}
-		logger.info("------------------修改重算状态总耗时：" + totalRetry + "毫秒------------------");
+		logger.info(taskId+"------------------修改重算状态总耗时：" + totalRetry + "毫秒------------------");
 		long totalEnd = System.currentTimeMillis();
-		logger.info("------------------修改重算状态耗时占比:" + totalRetry*1.0/(totalEnd-totalStart)*100 + "%------------------");
+		logger.info(taskId+"------------------修改重算状态耗时占比:" + totalRetry*1.0/(totalEnd-totalStart)*100 + "%------------------");
 		updateProgress(taskVo, 60);
 		return "sucess";
 	}
@@ -494,7 +503,7 @@ public class BmsProductTaskListener implements MessageListener{
 		condition.put("taskId", taskId);
 		List<BmsCorrectAsynTaskVo> taskList=bmsCorrectAsynTaskService.queryList(condition);
 		if(taskList.size()<=0){
-			logger.info("保温袋纠正,没有查询到任务记录;");
+			logger.info(taskId+"保温袋纠正,没有查询到任务记录;");
 			errorMessage.append("保温袋纠正,没有查询到任务记录;");
 			return;
 		}
@@ -502,7 +511,7 @@ public class BmsProductTaskListener implements MessageListener{
 		try{
 			String result=handBwd(taskVo,taskId,errorMessage);		
 			if("sucess".equals(result)){
-				logger.info("保温袋调整成功");
+				logger.info(taskId+"保温袋调整成功");
 				taskVo.setTaskRate(100);
 				errorMessage.append("保温袋调整成功;");
 				taskVo.setTaskStatus(BmsCorrectAsynTaskStatusEnum.SUCCESS.getCode());
@@ -519,7 +528,7 @@ public class BmsProductTaskListener implements MessageListener{
 				bmsCorrectAsynTaskService.update(taskVo);				
 			}
 		} catch (Exception e) {
-			logger.error("保温袋调整异常，错误日志：{}",e);
+			logger.error(taskId+"保温袋调整异常，错误日志：{}",e);
 			errorMessage.append("保温袋调整异常;");
 			taskVo.setRemark(errorMessage.toString());
 			taskVo.setTaskRate(95);
@@ -544,7 +553,7 @@ public class BmsProductTaskListener implements MessageListener{
 		Map<String,Object> condition=new HashMap<String,Object>();
 		if(StringUtils.isNotBlank(taskVo.getCustomerId())){
 			//根据商家和时间查询耗材出库表业务数据里的运单号
-			logger.info("查询保温袋汇总统计");
+			logger.info(taskId+"查询保温袋汇总统计");
 			condition=new HashMap<String,Object>();
 			condition.put("customerId", taskVo.getCustomerId());
 			condition.put("startTime", DateUtil.formatTimestamp(taskVo.getStartDate()));
@@ -552,22 +561,22 @@ public class BmsProductTaskListener implements MessageListener{
 			condition.put("taskId", taskId);
 			condition.put("type", "BWD");
 			
-			logger.info("插入保温袋汇总统计");
+			logger.info(taskId+"插入保温袋汇总统计"+JSONObject.fromObject(condition));
 			//插入汇总统计
 			int re=bmsProductsMaterialService.saveBwd(condition);
 			if(re<=0){
 				errorMessage.append("保温袋汇总统计失败;");
-				logger.info("保温袋汇总统计失败");
+				logger.info(taskId+"保温袋汇总统计失败");
 				return "fail";
 			}
 			updateProgress(taskVo, 80);
-			logger.info("获取占比最高的保温袋");
+			logger.info(taskId+"获取占比最高的保温袋");
 			//获取占比最高
 			condition.put("taskId", taskId);
 			start = System.currentTimeMillis();
 			List<BmsProductsMaterialAccountVo> list=bmsProductsMaterialService.queyAllBwxMax(condition);
 			end = System.currentTimeMillis();
-			logger.info("------------------获取占比最高耗时：" + (end-start) + "毫秒------------------");
+			logger.info(taskId+"------------------获取占比最高耗时：" + (end-start) + "毫秒------------------");
 			//循环判断是否有重复之
 			if(list.size()>0){
 				List<BizOutstockPackmaterialEntity> newList=new ArrayList<BizOutstockPackmaterialEntity>();				
@@ -588,7 +597,7 @@ public class BmsProductTaskListener implements MessageListener{
 						start = System.currentTimeMillis();
 						BizOutstockPackmaterialEntity markVo=bmsProductsMaterialService.queryBwdMaterial(condition);
 						end = System.currentTimeMillis();
-						logger.info("------------------查询出该商品明细下使用最多的耗材记录耗时：" + (end-start) + "毫秒------------------");
+						logger.info(taskId+"------------------查询出该商品明细下使用最多的耗材记录耗时：" + (end-start) + "毫秒------------------");
 						if(markVo!=null){
 							//将占比最高的运单号更新到汇总表里
 							condition=new HashMap<String,Object>();
@@ -601,9 +610,9 @@ public class BmsProductTaskListener implements MessageListener{
 							start = System.currentTimeMillis();
 							int updateResult=bmsProductsMaterialService.updateMaterialAccount(condition);
 							end = System.currentTimeMillis();
-							logger.info("------------------将占比最高的运单号更新到汇总表里耗时：" + (end-start) + "毫秒------------------");
+							logger.info(taskId+"------------------将占比最高的运单号更新到汇总表里耗时：" + (end-start) + "毫秒------------------");
 							if(updateResult<=0){
-								logger.info("更新汇总表失败"+condition);
+								logger.info(taskId+"更新汇总表失败"+condition);
 								continue;
 							}
 							
@@ -615,9 +624,10 @@ public class BmsProductTaskListener implements MessageListener{
 							condition.put("endTime", DateUtil.formatTimestamp(taskVo.getEndDate()));
 							condition.put("customerId", taskVo.getCustomerId());
 							start = System.currentTimeMillis();
+							logger.info("找出未使用标准的运单号参数"+JSONObject.fromObject(condition));
 							List<BizOutstockPackmaterialEntity> notMaxList=bmsProductsMaterialService.queyNotMaxBwd(condition);
 							end = System.currentTimeMillis();
-							logger.info("------------------找出未使用标准的运单号耗时：" + (end-start) + "毫秒------------------");
+							logger.info(taskId+"------------------找出未使用标准的运单号耗时：" + (end-start) + "毫秒------------------");
 							
 							for(int j=0,lenth=notMaxList.size();j<lenth;j++){
 								BizOutstockPackmaterialEntity entity=notMaxList.get(j);							
@@ -654,7 +664,7 @@ public class BmsProductTaskListener implements MessageListener{
 			}
 		}
 		long totalEnd = System.currentTimeMillis();
-		logger.info("------------------修改保温袋总耗时:" + totalRetry*1.0/(totalEnd-totalStart)*100 + "%------------------");
+		logger.info(taskId+"------------------修改保温袋总耗时:" + totalRetry*1.0/(totalEnd-totalStart)*100 + "%------------------");
 		updateProgress(taskVo, 90);
 		return "sucess";
 	}
