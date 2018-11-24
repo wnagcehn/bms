b3ea688fc5dfc2e3d924e7d691d222f1bdff7f66
wangchen870 <wangchen870@jiuyescm.com>
2018-11-24 13:44:55 +0800

    导入账单界面修改

--- bms-entity/src/main/java/com/jiuyescm/bms/bill/receive/entity/BillReceiveMasterEntity.java
+++ bms-entity/src/main/java/com/jiuyescm/bms/bill/receive/entity/BillReceiveMasterEntity.java
@@ -1,6 +1,7 @@
 package com.jiuyescm.bms.bill.receive.entity;
 
 import java.sql.Timestamp;
+import java.util.Date;
 import java.math.BigDecimal;
 import com.jiuyescm.cfm.domain.IEntity;
 
@@ -73,7 +74,37 @@ public class BillReceiveMasterEntity implements IEntity {
 	private String billCheckStatus;
 	
 	private String isneedInvoice;
-
+	//业务启动时间
+	private Date billStartTime;
+	//一级品类
+	private String firstClassName;
+	//业务类型
+	private String bizTypeName;
+	//项目
+	private String projectName;
+	//销售员id
+	private String sellerId;
+	//销售员名称
+	private String sellerName;
+	//部门名称
+	private String deptName;
+	//部门ID
+	private String deptCode;
+	//项目管理员id
+	private String projectManagerId;
+	//项目管理员
+	private String projectManagerName;
+	//结算员id
+	private String balanceId;
+	//结算员
+	private String balanceName;
+	//确认人ID
+	private String confirmManId;
+	//确认人
+	private String confirmMan;
+	//确认日期
+	private Date confirmDate;
+	
 	public BillReceiveMasterEntity() {
 		super();
 	}
@@ -312,6 +343,126 @@ public class BillReceiveMasterEntity implements IEntity {
 
 	public void setInvoiceId(String invoiceId) {
 		this.invoiceId = invoiceId;
+	}
+
+	public Date getBillStartTime() {
+		return billStartTime;
+	}
+
+	public void setBillStartTime(Date billStartTime) {
+		this.billStartTime = billStartTime;
+	}
+
+	public String getFirstClassName() {
+		return firstClassName;
+	}
+
+	public void setFirstClassName(String firstClassName) {
+		this.firstClassName = firstClassName;
+	}
+
+	public String getBizTypeName() {
+		return bizTypeName;
+	}
+
+	public void setBizTypeName(String bizTypeName) {
+		this.bizTypeName = bizTypeName;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(String sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	public String getDeptCode() {
+		return deptCode;
+	}
+
+	public void setDeptCode(String deptCode) {
+		this.deptCode = deptCode;
+	}
+
+	public String getProjectManagerId() {
+		return projectManagerId;
+	}
+
+	public void setProjectManagerId(String projectManagerId) {
+		this.projectManagerId = projectManagerId;
+	}
+
+	public String getProjectManagerName() {
+		return projectManagerName;
+	}
+
+	public void setProjectManagerName(String projectManagerName) {
+		this.projectManagerName = projectManagerName;
+	}
+
+	public String getBalanceId() {
+		return balanceId;
+	}
+
+	public void setBalanceId(String balanceId) {
+		this.balanceId = balanceId;
+	}
+
+	public String getBalanceName() {
+		return balanceName;
+	}
+
+	public void setBalanceName(String balanceName) {
+		this.balanceName = balanceName;
+	}
+
+	public String getConfirmManId() {
+		return confirmManId;
+	}
+
+	public void setConfirmManId(String confirmManId) {
+		this.confirmManId = confirmManId;
+	}
+
+	public String getConfirmMan() {
+		return confirmMan;
+	}
+
+	public void setConfirmMan(String confirmMan) {
+		this.confirmMan = confirmMan;
+	}
+
+	public Date getConfirmDate() {
+		return confirmDate;
+	}
+
+	public void setConfirmDate(Date confirmDate) {
+		this.confirmDate = confirmDate;
 	}
     
 }
