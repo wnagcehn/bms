8b38d3a79d941500b4e9ff30b0a5fa2cf2fd5db5
unknown <caojianwei@sh-jiuye.com.cn>
2018-11-27 11:52:17 +0800

    修改 bms dubbo  仓库查询bug

--- bms-bpi-impl/src/main/java/com/jiuyescm/bms/base/dict/WarehouseDictService.java
+++ bms-bpi-impl/src/main/java/com/jiuyescm/bms/base/dict/WarehouseDictService.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
 
 import com.jiuyescm.bms.base.dict.api.IWarehouseDictService;
 import com.jiuyescm.constants.RedisCache;
