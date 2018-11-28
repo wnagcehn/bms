c8efd01b70fbfcbd473aa98f62418f4c10456db1
liuzhicheng <liuzhicheng@jiuyescm.com>
2018-11-28 15:35:12 +0800

    干线账单导入

--- bms-mq-billimport/src/main/java/com/jiuyescm/bms/billimport/handler/TransportHandler.java
+++ bms-mq-billimport/src/main/java/com/jiuyescm/bms/billimport/handler/TransportHandler.java
@@ -15,6 +15,7 @@ import com.jiuyescm.bms.billimport.service.IBillFeesReceiveTransportTempService;
 import com.jiuyescm.bms.excel.data.DataColumn;
 import com.jiuyescm.bms.excel.data.DataRow;
 import com.jiuyescm.common.utils.DateUtil;
+import com.jiuyescm.constants.BmsEnums;
 import com.jiuyescm.exception.BizException;
 
 /**
@@ -82,8 +83,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 				case "温控":
 					if (StringUtils.isNotBlank(dc.getColValue())) {
 						entity.setTemperatureType(dc.getColValue());
-						
-//						entity.setTemperatureCode( );
+						entity.setTemperatureCode(BmsEnums.tempretureType.getCode(dc.getColValue()));
 					}
 					break;
 				case "派车单号":
@@ -185,6 +185,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "卸货费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity2, entity);
+					entity2.setAmount(new BigDecimal(dc.getColValue()));
 					entity2.setSubjectCode("YS_XH");
 					entity2.setSubjectName("卸货费");
 					listEntity.add(entity2);	
@@ -193,6 +194,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "装货费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity3, entity);
+					entity3.setAmount(new BigDecimal(dc.getColValue()));
 					entity3.setSubjectCode("YS_ZH");
 					entity3.setSubjectName("装货费");
 					listEntity.add(entity3);
@@ -201,6 +203,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "缠绕膜费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity4, entity);
+					entity4.setAmount(new BigDecimal(dc.getColValue()));
 					entity4.setSubjectCode("YS_CRM");
 					entity4.setSubjectName("缠绕膜费");
 					listEntity.add(entity4);
@@ -209,6 +212,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "纸面回单费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity5, entity);
+					entity5.setAmount(new BigDecimal(dc.getColValue()));
 					entity5.setSubjectCode("YS_ZMHD");
 					entity5.setSubjectName("纸面回单费");
 					listEntity.add(entity5);
@@ -217,6 +221,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "逆向物流费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity6, entity);
+					entity6.setAmount(new BigDecimal(dc.getColValue()));
 					entity6.setSubjectCode("YS_NXWL");
 					entity6.setSubjectName("逆向物流费");
 					listEntity.add(entity6);
@@ -225,6 +230,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "拆箱费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity7, entity);
+					entity7.setAmount(new BigDecimal(dc.getColValue()));
 					entity7.setSubjectCode("YS_CX");
 					entity7.setSubjectName("拆箱费");
 					listEntity.add(entity7);	
@@ -233,6 +239,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "贴码费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity8, entity);
+					entity8.setAmount(new BigDecimal(dc.getColValue()));
 					entity8.setSubjectCode("YS_TM");
 					entity8.setSubjectName("贴码费");
 					listEntity.add(entity8);	
@@ -241,6 +248,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "分流费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity9, entity);
+					entity9.setAmount(new BigDecimal(dc.getColValue()));
 					entity9.setSubjectCode("YS_FL");
 					entity9.setSubjectName("分流费");
 					listEntity.add(entity9);
@@ -249,6 +257,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "上楼搬运费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity10, entity);
+					entity10.setAmount(new BigDecimal(dc.getColValue()));
 					entity10.setSubjectCode("YS_SLBY");
 					entity10.setSubjectName("上楼搬运费");
 					listEntity.add(entity10);	
@@ -257,6 +266,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "等待制冷费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity11, entity);
+					entity11.setAmount(new BigDecimal(dc.getColValue()));
 					entity11.setSubjectCode("YS_DD");
 					entity11.setSubjectName("等待制冷费");
 					listEntity.add(entity11);	
@@ -265,6 +275,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "过夜制冷费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity12, entity);
+					entity12.setAmount(new BigDecimal(dc.getColValue()));
 					entity12.setSubjectCode("YS_GY");
 					entity12.setSubjectName("过夜制冷费");
 					listEntity.add(entity12);
@@ -273,6 +284,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "放空费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity13, entity);
+					entity13.setAmount(new BigDecimal(dc.getColValue()));
 					entity13.setSubjectCode("YS_FK");
 					entity13.setSubjectName("放空费");
 					listEntity.add(entity13);	
@@ -281,6 +293,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "单据打印费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity14, entity);
+					entity14.setAmount(new BigDecimal(dc.getColValue()));
 					entity14.setSubjectCode("YS_DJDY");
 					entity14.setSubjectName("单据打印费");
 					listEntity.add(entity14);	
@@ -289,6 +302,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "提货费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity15, entity);
+					entity15.setAmount(new BigDecimal(dc.getColValue()));
 					entity15.setSubjectCode("YS_TH");
 					entity15.setSubjectName("提货费");
 					listEntity.add(entity15);
@@ -297,6 +311,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "送货费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity16, entity);
+					entity16.setAmount(new BigDecimal(dc.getColValue()));
 					entity16.setSubjectCode("YS_SH");
 					entity16.setSubjectName("送货费");
 					listEntity.add(entity16);	
@@ -305,6 +320,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "加点费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity17, entity);
+					entity17.setAmount(new BigDecimal(dc.getColValue()));
 					entity17.setSubjectCode("YS_JD");
 					entity17.setSubjectName("加点费");
 					listEntity.add(entity17);	
@@ -313,6 +329,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "赔付费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity18, entity);
+					entity18.setAmount(new BigDecimal(dc.getColValue()));
 					entity18.setSubjectCode("YS_PF");
 					entity18.setSubjectName("赔付费");
 					listEntity.add(entity18);
@@ -321,6 +338,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "中转费":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity19, entity);
+					entity19.setAmount(new BigDecimal(dc.getColValue()));
 					entity19.setSubjectCode("YS_ZZ");
 					entity19.setSubjectName("中转费");
 					listEntity.add(entity19);	
@@ -329,6 +347,7 @@ public class TransportHandler extends CommonHandler<BillFeesReceiveTransportTemp
 			case "其他":
 				if (StringUtils.isNotBlank(dc.getColValue())) {
 					PropertyUtils.copyProperties(entity20, entity);
+					entity20.setAmount(new BigDecimal(dc.getColValue()));
 					entity20.setSubjectCode("YS_QT");
 					entity20.setSubjectName("其他");
 					listEntity.add(entity20);
