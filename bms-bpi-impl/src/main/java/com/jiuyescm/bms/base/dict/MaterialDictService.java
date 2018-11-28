438c1988a8af8f51945abcd4e5f15df6c2706572
unknown <caojianwei@sh-jiuye.com.cn>
2018-11-28 13:37:42 +0800

    bms dubbo 新增耗材查询服务 新增redis缓存 代码优化

--- bms-bpi-impl/src/main/java/com/jiuyescm/bms/base/dict/MaterialDictService.java
+++ bms-bpi-impl/src/main/java/com/jiuyescm/bms/base/dict/MaterialDictService.java
@@ -88,7 +88,7 @@ public class MaterialDictService implements IMaterialDictService {
 				condition.put("barcode", code);
 				List<PubMaterialInfoVo> list =  pubMaterialInfoService.queryList(condition);
 				if(list == null || list.size()==0){
-					return null;
+					return new PubMaterialInfoVo();
 				}
 				return list.get(0);
 			}
@@ -112,7 +112,7 @@ public class MaterialDictService implements IMaterialDictService {
 				condition.put("materialName", name);
 				List<PubMaterialInfoVo> list =  pubMaterialInfoService.queryList(condition);
 				if(list == null || list.size()==0){
-					return null;
+					return new PubMaterialInfoVo();
 				}
 				return list.get(0);
 			}
