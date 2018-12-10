c9f4f986dbec4a2e0939be7ee8e24745ac3a0b51
zhaofeng <zhaofeng@jiuyescm.com>
2018-12-10 11:24:29 +0800

    报表导出修改

--- bms-site/src/main/java/com/jiuyescm/bms/base/delivery/web/DeliveryController.java
+++ bms-site/src/main/java/com/jiuyescm/bms/base/delivery/web/DeliveryController.java
@@ -57,42 +57,110 @@ public class DeliveryController {
 		parameter.put("excelName", "账单报表导出模板.xlsx");
 		
 		// TODO Auto-generated method stub
-		List<Map<String,Object>> list1=new ArrayList<Map<String,Object>>();
-		Map<String,Object> map1=new HashMap<String,Object>();
-		map1.put("createMonth", "1809");
-		map1.put("invoiceName", "北京行知守仁科技有限公司");
-		map1.put("billName", "北京行知守仁科技有限公司");
-		map1.put("billStartTime", JAppContext.currentTimestamp());
-		Map<String,Object> map2=new HashMap<String,Object>();
-		map2.put("createMonth", "1801");
-		map2.put("invoiceName", "上海乐亲电子商务有限公司");
-		map2.put("billName", "上海乐亲电子商务有限公司");
-		map2.put("unInvoiceAmount", 12.45);
-		list1.add(map1);	
-		list1.add(map2);
+/*		*/
 		
 		final String fileName=parameter.get("excelName");
 		byte[] bytes=storageClient.downloadFile(parameter.get("url"),new DownloadByteArray());
 		try{
 			XSSFWorkbook xssfWorkbook = new XSSFWorkbook(new ByteArrayInputStream(bytes));
 			
-			//第一个sheet写数据
-		    XSSFSheet sheet0=xssfWorkbook.getSheetAt(0);
-		    XSSFRow row=sheet0.getRow(1);
-		    Iterator<Cell> cellList=row.iterator();	
+			//=================================第一个sheet写数据===========================
+			List<Map<String,Object>> list1=new ArrayList<Map<String,Object>>();
+			Map<String,Object> map1=new HashMap<String,Object>();
+			map1.put("createMonth", "1809");
+			map1.put("invoiceName", "北京行知守仁科技有限公司");
+			map1.put("billName", "北京行知守仁科技有限公司");
+			map1.put("billStartTime", JAppContext.currentTimestamp());
+			Map<String,Object> map2=new HashMap<String,Object>();
+			map2.put("createMonth", "1801");
+			map2.put("invoiceName", "上海乐亲电子商务有限公司");
+			map2.put("billName", "上海乐亲电子商务有限公司");
+			map2.put("unInvoiceAmount", 12.45);
+			list1.add(map1);	
+			list1.add(map2);
+			
+		    XSSFSheet sheet1=xssfWorkbook.getSheetAt(0);
+		    XSSFRow firstRow1=sheet1.getRow(1);
+		    Iterator<Cell> cellList1=firstRow1.iterator();	
 		    //第一行对应得字段
-		    List<Cell> myList=IteratorUtils.toList(cellList);		    		    
+		    List<Cell> myList1=IteratorUtils.toList(cellList1);		    		    
 		    for(int i=0;i<list1.size();i++){
 		    	Map<String,Object> map=list1.get(i);
-		    	XSSFRow newrow =sheet0.createRow(i+2);
-		    	for(Cell cell:myList){
+		    	XSSFRow newrow =sheet1.createRow(i+2);
+		    	for(Cell cell:myList1){
 		    		if(map.containsKey(cell.getStringCellValue())){
 		    			newrow.createCell(cell.getColumnIndex()).setCellValue(map.get(cell.getStringCellValue()).toString());
 		    		}
 		    	}
 		    }		    
-		    sheet0.shiftRows(row.getRowNum()+1, row.getRowNum()+1+list1.size(), -1);//删除第一行，然后使下方单元格上移
-		       
+		    sheet1.shiftRows(firstRow1.getRowNum()+1, firstRow1.getRowNum()+1+list1.size(), -1);//删除第一行，然后使下方单元格上移
+		    
+		    
+			//=================================第二个sheet写数据===========================
+		    List<Map<String,Object>> list2=new ArrayList<Map<String,Object>>();
+			Map<String,Object> map21=new HashMap<String,Object>();
+			map21.put("createMonth", "1809");
+			map21.put("invoiceName", "北京我爱小城信息科技有限公司");
+			map21.put("billName", "北京我爱小城信息科技有限公司");
+			map21.put("invoiceStorage", 11.45);
+			Map<String,Object> map22=new HashMap<String,Object>();
+			map22.put("createMonth", "1801");
+			map22.put("invoiceName", "新疆晨报苹果（个人）");
+			map22.put("billName", "新疆晨报苹果（个人）");
+			map22.put("shunfengCount", 12);
+			list2.add(map21);	
+			list2.add(map22);
+			//第二个sheet写数据
+		    XSSFSheet sheet2=xssfWorkbook.getSheetAt(1);
+		    XSSFRow firstRow2=sheet2.getRow(2);
+		    Iterator<Cell> cellList2=firstRow2.iterator();	
+		    //第一行对应得字段
+		    List<Cell> myList2=IteratorUtils.toList(cellList2);		    		    
+		    for(int i=0;i<list2.size();i++){
+		    	Map<String,Object> map=list2.get(i);
+		    	XSSFRow newrow =sheet2.createRow(i+3);
+		    	for(Cell cell:myList2){
+		    		if(map.containsKey(cell.getStringCellValue())){
+		    			newrow.createCell(cell.getColumnIndex()).setCellValue(map.get(cell.getStringCellValue()).toString());
+		    		}
+		    	}
+		    }		    
+		    sheet2.shiftRows(firstRow2.getRowNum()+1, firstRow2.getRowNum()+1+list2.size(), -1);//删除第一行，然后使下方单元格上移
+		   
+			//=================================第三个sheet写数据===========================
+		    List<Map<String,Object>> list3=new ArrayList<Map<String,Object>>();
+			Map<String,Object> map31=new HashMap<String,Object>();
+			map31.put("createMonth", "1809");
+			map31.put("invoiceName", "深圳壹心贸易有限公司");
+			map31.put("billName", "深圳壹心贸易有限公司");
+			map31.put("abnormalDispatch", 11.45);
+			Map<String,Object> map32=new HashMap<String,Object>();
+			map32.put("createMonth", "1801");
+			map32.put("invoiceName", "新疆叫了只炸鸡");
+			map32.put("billName", "新疆叫了只炸鸡");
+			map32.put("shunfengCount", 12);
+			map32.put("transferPallet", 12);	
+			list3.add(map31);	
+			list3.add(map32);
+			//第二个sheet写数据
+		    XSSFSheet sheet3=xssfWorkbook.getSheetAt(2);
+		    XSSFRow firstRow3=sheet3.getRow(3);
+		    Iterator<Cell> cellList3=firstRow3.iterator();	
+		    //第一行对应得字段
+		    List<Cell> myList3=IteratorUtils.toList(cellList3);		    		    
+		    for(int i=0;i<list3.size();i++){
+		    	Map<String,Object> map=list3.get(i);
+		    	XSSFRow newrow =sheet3.createRow(i+4);
+		    	for(Cell cell:myList3){
+		    		if(map.containsKey(cell.getStringCellValue())){
+		    			newrow.createCell(cell.getColumnIndex()).setCellValue(map.get(cell.getStringCellValue()).toString());
+		    		}
+		    	}
+		    }		    
+		    sheet3.shiftRows(firstRow3.getRowNum()+1, firstRow3.getRowNum()+1+list3.size(), -1);//删除第一行，然后使下方单元格上移
+		   
+		    
+		    
 			ByteArrayOutputStream os = new ByteArrayOutputStream();
 			xssfWorkbook.write(os);
 			byte[] b1 = os.toByteArray();
