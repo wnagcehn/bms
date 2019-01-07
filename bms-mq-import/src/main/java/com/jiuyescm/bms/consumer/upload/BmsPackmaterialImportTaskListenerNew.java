79d5b1e83edba42e146dc689d7549a19d638388e
wangchen870 <wangchen870@jiuyescm.com>
2019-01-07 17:40:41 +0800

    删除多余文件

--- bms-mq-import/src/main/java/com/jiuyescm/bms/consumer/upload/BmsPackmaterialImportTaskListenerNew.java
+++ bms-mq-import/src/main/java/com/jiuyescm/bms/consumer/upload/BmsPackmaterialImportTaskListenerNew.java
@@ -12,8 +12,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
