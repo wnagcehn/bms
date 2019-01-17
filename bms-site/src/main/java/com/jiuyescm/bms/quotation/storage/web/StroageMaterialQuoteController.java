c465758716d6d936717b9054b090d22998074c28
wangchen870 <wangchen870@jiuyescm.com>
2019-01-17 15:58:56 +0800

    耗材报价导入bug修改

--- bms-site/src/main/java/com/jiuyescm/bms/quotation/storage/web/StroageMaterialQuoteController.java
+++ bms-site/src/main/java/com/jiuyescm/bms/quotation/storage/web/StroageMaterialQuoteController.java
@@ -142,7 +142,7 @@ public class StroageMaterialQuoteController extends CommonComparePR<PriceMateria
 				parameter.put("templateId", entity.getTemplateId());
 				List<PriceMaterialQuotationEntity> list = service.queryByTemplateId(parameter);
 				for (PriceMaterialQuotationEntity priceMaterialQuotationEntity : list) {
-					if ((priceMaterialQuotationEntity.getMaterialCode()+priceMaterialQuotationEntity.getWarehouseId()).equals(entity.getMaterialCode()+(entity.getWarehouseId()==null?"":entity.getWarehouseId()))) {
+					if ((priceMaterialQuotationEntity.getMaterialCode()+(priceMaterialQuotationEntity.getWarehouseId()==null?"":priceMaterialQuotationEntity.getWarehouseId())).equals(entity.getMaterialCode()+(entity.getWarehouseId()==null?"":entity.getWarehouseId()))) {
 						return "非泡沫箱的耗材不允许重复录入!";
 					}
 				}
@@ -205,7 +205,7 @@ public class StroageMaterialQuoteController extends CommonComparePR<PriceMateria
 				parameter.put("id", entity.getId());
 				List<PriceMaterialQuotationEntity> list = service.queryByTemplateId(parameter);
 				for (PriceMaterialQuotationEntity priceMaterialQuotationEntity : list) {
-					if ((priceMaterialQuotationEntity.getMaterialCode()+priceMaterialQuotationEntity.getWarehouseId()).equals(entity.getMaterialCode()+(entity.getWarehouseId()==null?"":entity.getWarehouseId()))) {
+					if ((priceMaterialQuotationEntity.getMaterialCode()+(priceMaterialQuotationEntity.getWarehouseId()==null?"":priceMaterialQuotationEntity.getWarehouseId())).equals(entity.getMaterialCode()+(entity.getWarehouseId()==null?"":entity.getWarehouseId()))) {
 						return "非泡沫箱的耗材不允许重复录入!";
 					}
 				}
@@ -712,6 +712,8 @@ public class StroageMaterialQuoteController extends CommonComparePR<PriceMateria
 						break;
 					}
 				}
+			}else if (StringUtils.isEmpty(p.getWarehouseId()) && "泡沫箱".equals(p.getMaterialType())) {
+				
 			}else {
 				for(WarehouseVo entity:wareHouselist)
 				{
