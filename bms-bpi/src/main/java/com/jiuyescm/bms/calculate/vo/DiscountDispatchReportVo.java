443e833c13a829dff934af0363d1e205f99e3bd5
unknown <caojianwei@sh-jiuye.com.cn>
2019-04-18 15:17:52 +0800

    #BMS-1254 新增折扣报价服务

---
+++ bms-bpi/src/main/java/com/jiuyescm/bms/calculate/vo/DiscountDispatchReportVo.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2019, Jiuye SCM and/or its affiliates. All rights reserved.
+ */
+package com.jiuyescm.bms.calculate.vo;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * <功能描述>
+ * 
+ * @author caojianwei
+ * @date 2019年4月18日 下午12:48:12
+ */
+public class DiscountDispatchReportVo {
+
+    private String serviceTypeCode;
+    private Integer sumNum;
+    private BigDecimal sumAmount;
+    List<DiscountQuoteVo> quotes;
+    private boolean isDiscount;
+    private boolean isRemove;
+    
+    /**
+     *物流产品类型
+     */
+    public String getServiceTypeCode() {
+        return serviceTypeCode;
+    }
+    
+    /**
+     *物流产品类型
+     */
+    public void setServiceTypeCode(String serviceTypeCode) {
+        this.serviceTypeCode = serviceTypeCode;
+    }
+    
+    /**
+     *总单量
+     */
+    public Integer getSumNum() {
+        return sumNum;
+    }
+    
+    /**
+     *总单量
+     */
+    public void setSumNum(Integer sumNum) {
+        this.sumNum = sumNum;
+    }
+    
+    /**
+     *总金额
+     */
+    public BigDecimal getSumAmount() {
+        return sumAmount;
+    }
+    
+    /**
+     *总金额
+     */
+    public void setSumAmount(BigDecimal sumAmount) {
+        this.sumAmount = sumAmount;
+    }
+    
+    /**
+     *折扣报价明细
+     */
+    public List<DiscountQuoteVo> getQuotes() {
+        return quotes;
+    }
+    
+    /**
+     *折扣报价明细
+     */
+    public void setQuotes(List<DiscountQuoteVo> quotes) {
+        this.quotes = quotes;
+    }
+    
+    /**
+     *是否折扣  true-折扣  false-不折扣
+     */
+    public boolean isDiscount() {
+        return isDiscount;
+    }
+    
+    /**
+     *是否折扣  true-折扣  false-不折扣
+     */
+    public void setDiscount(boolean isDiscount) {
+        this.isDiscount = isDiscount;
+    }
+
+    /**
+     * 是否从上级单量中排除此级单量 true-排除  false-不排除
+     */
+    public boolean isRemove() {
+        return isRemove;
+    }
+
+    /**
+     * 是否从上级单量中排除此级单量 true-排除  false-不排除
+     */
+    public void setRemove(boolean isRemove) {
+        this.isRemove = isRemove;
+    }
+}
+
+
