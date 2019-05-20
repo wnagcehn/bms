5ac2778f40669dacfcd58c602cf22af16f2894e5
liuzhicheng <liuzhicheng@jiuyescm.com>
2019-05-20 16:56:41 +0800

    #BMS-1469 纠正明细查询修改

--- bms-site/src/main/java/com/jiuyescm/bms/correct/controller/BmsOrderProductController.java
+++ bms-site/src/main/java/com/jiuyescm/bms/correct/controller/BmsOrderProductController.java
@@ -1,45 +1,62 @@
 package com.jiuyescm.bms.correct.controller;
 
+import java.util.List;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.alibaba.dubbo.common.utils.CollectionUtils;
 import com.bstek.dorado.annotation.DataProvider;
-import com.bstek.dorado.annotation.DataResolver;
 import com.bstek.dorado.data.provider.Page;
 import com.github.pagehelper.PageInfo;
-import com.jiuyescm.bms.correct.BmsOrderProductEntity;
+import com.jiuyescm.bms.correct.service.IBmsMarkingProductsService;
 import com.jiuyescm.bms.correct.service.IBmsOrderProductService;
+import com.jiuyescm.bms.correct.vo.BmsMarkingProductsVo;
 import com.jiuyescm.bms.correct.vo.BmsOrderProductVo;
 
 /**
  * ..Controller
+ * 
  * @author wangchen
  * 
  */
 @Controller("bmsOrderProductController")
 public class BmsOrderProductController {
 
-	@Autowired
-	private IBmsOrderProductService bmsOrderProductService;
+    @Autowired
+    private IBmsOrderProductService bmsOrderProductService;
+    @Autowired
+    private IBmsMarkingProductsService bmsMarkingProductsService;
 
-
-	/**
-	 * 分页查询
-	 * @param page
-	 * @param param
-	 * @throws Exception 
-	 */
-	@DataProvider
-	public void query(Page<BmsOrderProductVo> page, Map<String, Object> param) throws Exception {
-		PageInfo<BmsOrderProductVo> pageInfo = bmsOrderProductService.query(param, page.getPageNo(), page.getPageSize());
-		if (pageInfo != null) {
-			page.setEntities(pageInfo.getList());
-			page.setEntityCount((int) pageInfo.getTotal());
-		}
-	}
+    /**
+     * 分页查询
+     * 
+     * @param page
+     * @param param
+     * @throws Exception
+     */
+    @DataProvider
+    public void query(Page<BmsOrderProductVo> page, Map<String, Object> param) throws Exception {
+        String waybillNo = (String) param.get("waybillNo");
+        //使用运单号查找
+        if (StringUtils.isNotBlank(waybillNo)) {
+            //查询运单号的products_mark
+            PageInfo<BmsMarkingProductsVo> pageInfo = bmsMarkingProductsService.query(param, 1, 20);
+            List<BmsMarkingProductsVo> list = pageInfo.getList();
+            if(!CollectionUtils.isEmpty(list)){
+                param.put("waybillNo", "");
+                param.put("productMark", list.get(0).getProductsMark());
+            }
+            
+        }
+        PageInfo<BmsOrderProductVo> pageInfo = bmsOrderProductService
+                .query(param, page.getPageNo(), page.getPageSize());
+        if (pageInfo != null) {
+            page.setEntities(pageInfo.getList());
+            page.setEntityCount((int) pageInfo.getTotal());
+        }
+    }
 
 }
