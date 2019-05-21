7c9dfa3d04b3c605d264f0e754c573d785daa0cb
liuzhicheng <liuzhicheng@jiuyescm.com>
2019-05-21 14:15:47 +0800

    #BMS-1469 增值界面修改

--- bms-site/src/main/java/com/jiuyescm/bms/base/group/web/BmsGroupController.java
+++ bms-site/src/main/java/com/jiuyescm/bms/base/group/web/BmsGroupController.java
@@ -13,6 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 
 import com.bstek.dorado.annotation.DataProvider;
 import com.bstek.dorado.annotation.DataResolver;
@@ -534,4 +535,29 @@ public class BmsGroupController {
 		
 		return result;
 	}
+
+   @DataProvider
+    public Map<String,String> getAddFeeSubject(String groupCode){
+        return bmsGroupSubjectService.getWmsValueAddSubjectGroups();
+    }
+
+   @DataProvider
+   public List<Map<String, String>> getAddFeeType(Map<String, Object> parameter) {
+       String firstSubject = (String) parameter.get("firstSubject");
+       if(StringUtils.isBlank(firstSubject)){
+           return null;
+       }
+       Map<String, String> map = bmsGroupSubjectService.getWmsValueAddSubjectDetails(firstSubject);
+       if(CollectionUtils.isEmpty(map)){
+           return null;
+       }
+       List<Map<String, String>> list = new ArrayList<>();
+       for (String key : map.keySet()) {
+           Map<String, String> bean = new HashMap<>();
+           bean.put("feesType", key);
+           bean.put("feesTypeName", map.get(key));
+           list.add(bean);
+        }
+       return list;
+   }
 }
