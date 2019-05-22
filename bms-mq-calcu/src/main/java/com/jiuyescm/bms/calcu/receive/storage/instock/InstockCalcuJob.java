b3eb4bffb3c8cf73e75c296b663cf65c5e70321d
unknown <zhaofeng@sh-jiuye.com.cn>
2019-05-22 17:26:38 +0800

    #BMS-1476 合同生效日期、失效日期修改

--- bms-mq-calcu/src/main/java/com/jiuyescm/bms/calcu/receive/storage/instock/InstockCalcuJob.java
+++ bms-mq-calcu/src/main/java/com/jiuyescm/bms/calcu/receive/storage/instock/InstockCalcuJob.java
@@ -201,7 +201,7 @@ public class InstockCalcuJob extends BmsContractBase implements ICalcuService<Bm
         //合同
         CalcuContractVo contract=null;
         for(CalcuContractVo con:contractList){
-            if(con.getStartDate().before(entity.getCreateTime()) && entity.getCreateTime().before(con.getExpireDate())){
+            if(con.getStartDate().getTime()<=entity.getCreateTime().getTime() && entity.getCreateTime().getTime()<=con.getExpireDate().getTime()){
                 contract=con;
                 break;
             }
